f1761c2e768ad3811da582a77d4ece6527b64f53
Chris Jones <chris@chris.local>
2019-12-08 17:05:05 -0800

    only save the primary term if assigned to the post

--- includes/functions/save_post.php
+++ includes/functions/save_post.php
@@ -14,7 +14,8 @@ function save_post( $post_id, $post ) {
     foreach( $_POST as $key => $value ) {
         // If the key starts with our prefix...
         if( strpos( $key, 'primary-term_' ) === 0 ) {
-            if( $value ) {
+            $taxonomy = split( $key, 'primary-term_' )[0];
+            if( $value && has_term( $value, $taxonomy, $post ) ) {
                 // ... then lets sanitize the value and save it...
                 $sanitized_value = sanitize_meta( $key, $value, 'post', $post->post_type );
                 update_post_meta( $post_id, $key, $sanitized_value );
